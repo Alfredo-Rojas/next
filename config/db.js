30590c3de5e1a3035b6425adf58dd98ea637ebd7
Alfredo-Rojas <alfredot84@gmail.com>
2019-08-14 12:50:58 -0400

    deployoing

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
